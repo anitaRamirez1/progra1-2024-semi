acd003345a97f511aeb4f68b52ffb4f717291819
ANA ELISA RAMIREZ PORTILLO <eliayala1105@gmail.com>
2024-08-26 16:56:04 -0600

    mi primer proyecto

---
+++ MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hola mundo");
+            //pausa
+
+            Console.ReadLine();
+           
+                }
+    }
+}
