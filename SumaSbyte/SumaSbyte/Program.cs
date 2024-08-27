73bf581b42d5b324fd7d5a6d6d87da81e4f46f58
ANA ELISA RAMIREZ PORTILLO <eliayala1105@gmail.com>
2024-08-26 19:48:31 -0600

    tipos de datos en c#

--- SumaSbyte/SumaSbyte/Program.cs
+++ SumaSbyte/SumaSbyte/Program.cs
@@ -12,24 +12,25 @@ namespace SumaSbyte
         {
 
             Console.Write("Num1: ");
-            int num1 = int.Parse(Console.ReadLine());
             sbyte num1 = sbyte.Parse(Console.ReadLine());
-
-            Console.Write("Num 2: ");
-            int num2 = int.Parse(Console.ReadLine());
+            Boolean comprobar = true;
+            Console.WriteLine("El valor es: {0}", comprobar);
 
             Console.Write("Num2: ");
-
             sbyte num2 = sbyte.Parse(Console.ReadLine());
+            Console.Write("Nombre: ");
+            string nombre = Console.ReadLine();
 
-            int respuesta = num1 + num2;
-            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
             int suma = num1 + num2;
 
             Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, suma);
+            Console.WriteLine("Hola {0} bienvenido", nombre);
 
-          //  Pausa.
-          Console.ReadLine();
+            DateTime fechaActual = new DateTime();
+         
+            Console.WriteLine("La fecha actual es: {0}", fechaActual);
+            //Pausa.
+             Console.ReadLine();
         }
     }
 }
