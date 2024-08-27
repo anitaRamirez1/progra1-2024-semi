057a5a16b750e9855d99e686cf55a2c1babaf89f
ANA ELISA RAMIREZ PORTILLO <eliayala1105@gmail.com>
2024-08-27 15:56:06 -0600

    prioridad de los operadores aritméticos

---
+++ prioridad de los operadores aritmeticos/prioridad de los operadores aritmeticos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prioridad_de_los_operadores_aritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Boolean comprobar = true;
+            Console.WriteLine("El valor es: {0}", comprobar);
+           // Prioridad de los operadores aritméticos.
+           // se pone el sufijo m para la division con decimales.
+           float respuesta  = 5 + 8 - 2 * 5 / 4m + 1; //14.75 valor incorrecto; 11.5->Valor correcto
+            Console.WriteLine("La respuesta es: {0}", respuesta);
+
+            Console.write("Nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola {0} bienvenido", nombre);
+
+            DateTime fechaActual = new DateTime();
+            fechaActual = FechaHora.Ahora;
+            Console.WriteLine("La fecha actual es: {0}", fechaActual);
+            Console.WriteLine("La division de 5/4 es {0}", 5 / 4);
+            //Pausa.
+           Console.ReadLine();
+
+        }
+    }
+}
