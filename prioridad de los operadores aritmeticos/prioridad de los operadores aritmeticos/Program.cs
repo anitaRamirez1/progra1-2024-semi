01f534b42b20d7609375c387abc236f91df42d27
ANA ELISA RAMIREZ PORTILLO <eliayala1105@gmail.com>
2024-08-27 18:24:12 -0600

    determinar si cada numero de la serie es par o impar

--- prioridad de los operadores aritmeticos/prioridad de los operadores aritmeticos/Program.cs
+++ prioridad de los operadores aritmeticos/prioridad de los operadores aritmeticos/Program.cs
@@ -10,16 +10,34 @@ namespace prioridad_de_los_operadores_aritmeticos
     {
         static void Main(string[] args)
         {
-            //Uso de matrices.
-            int[] numeros = new int[] { 2, 8, 9, 10, 1, 3 };
-            Console.WriteLine("El numero de la posicion 3 es {0}", numeros[3]);
-            for (int i = 0; i < numeros.Length; i++)
+
+            //obtener la secuencia fibonacci
+            int a = 0, b = 1, c = 1;
+            /*
+             a+b=c
+             0+1=1
+             1+0=1
+             1+1=2
+             2+1=3
+             3+2=5
+             5+3=8
+             8+5=13
+            ...
+             */
+            while (c < 100)
             {
-                Console.WriteLine("Indice: {0}={1}", i, numeros[i]);
+                Console.WriteLine("{0}+{1}={2}", a, b, c);
+                b = a;
+                a = c;
+                c = a + b;
+                //determinar si cada numero de la serie es para o impar.
+                int[] serie = new int[] { 5, 9, 4, 6, 3, 2 };
+                foreach (int num in serie)
+                {
+                    Console.WriteLine("El num {0} es {1}", num, num % 2 == 0 ? "Par" : "Impar");
+                }
+                Console.ReadLine();
             }
-            //Pausa
-            Console.ReadKey();
         }
     }
-
 }
