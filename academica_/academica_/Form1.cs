9dc70f4e4b917a9749f6022912caba125947a59c
ANA ELISA RAMIREZ PORTILLO <eliayala1105@gmail.com>
2024-09-11 10:38:56 -0600

    creacion de una base de datos db_academica y la tabla de alumnos

--- academica_/academica_/Form1.cs
+++ academica_/academica_/Form1.cs
@@ -16,5 +16,20 @@ namespace academica_
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCodigoAlumno_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
