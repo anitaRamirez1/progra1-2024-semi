4b2f6e39470469e29d81f1fe87178243ac2e94d0
ANA ELISA RAMIREZ PORTILLO <eliayala1105@gmail.com>
2024-10-05 14:43:41 -0600

    rama3

---
+++ sistema_academico/sistema_academico/Models/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace sistema_academico.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() { }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base  (options) { }
+        public DbSet<Alumno> Alumno { get; set; }
+
+        protected override void  OnModelCreating(ModelBuilder modelBuilder )
+        {
+          base.OnModelCreating (modelBuilder);
+            modelBuilder.Entity<Alumno>().HasKey( e => e.idAlumno );
+        }
+
+    }
+}
